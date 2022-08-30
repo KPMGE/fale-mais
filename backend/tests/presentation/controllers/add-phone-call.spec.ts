b09fea7a00ba1ac9f52e88e8a39a92d3a80a2e42
KPMGE <jesuskevin254@gmail.com>
2022-08-30 17:41:21 -0300

    :white_check_mark: ensure AddPhoneCallController returns added phone call on success

---
+++ backend/tests/presentation/controllers/add-phone-call.spec.ts
@@ -0,0 +1,35 @@
+import { PhoneCall } from "../../../src/domain/entities"
+import { AddPhoneCallUseCase } from "../../../src/domain/useCases"
+import { ok } from "../../../src/presentation/helpers"
+import { Controller, HttpResponse } from "../../../src/presentation/protocols"
+import { makeFakePhoneCall } from "../../domain/mocks"
+
+class AddPhoneCallServiceMock implements AddPhoneCallUseCase {
+  input = null
+  output = { ...makeFakePhoneCall(), id: 'some id' }
+  async add(newCall: AddPhoneCallUseCase.Props): Promise<PhoneCall> {
+    this.input = newCall
+    return this.output
+  }
+}
+
+class AddPhoneCallController implements Controller<AddPhoneCallUseCase.Props> {
+  constructor(private readonly service: AddPhoneCallUseCase) { }
+
+  async handle(req: AddPhoneCallUseCase.Props): Promise<HttpResponse> {
+    const addedPhoneCall = await this.service.add(req)
+    return ok(addedPhoneCall)
+  }
+}
+
+describe('add-phone-call-controller', () => {
+  it('should return added phone call on success', async () => {
+    const serviceMock = new AddPhoneCallServiceMock()
+    const sut = new AddPhoneCallController(serviceMock)
+
+    const httpResponse = await sut.handle(makeFakePhoneCall())
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual(serviceMock.output)
+  })
+})
