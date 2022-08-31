4f4601707cbd9fb08852a31a3cc6cf706df24c5a
KPMGE <jesuskevin254@gmail.com>
2022-08-31 17:33:31 -0300

    :recycle: implement GetPhoneCallByDDD on in-memory repository

--- backend/src/infrastructure/repositories/in-memory/in-memory-phone-call.ts
+++ backend/src/infrastructure/repositories/in-memory/in-memory-phone-call.ts
@@ -1,10 +1,10 @@
-import { ListPhoneCallsRepository } from "../../../application/repositories";
+import { GetPhoneCallByDDDRepository, ListPhoneCallsRepository } from "../../../application/repositories";
 import { AddPhoneCallRepository } from "../../../application/repositories/add-phone-call";
 import { PhoneCall } from "../../../domain/entities";
 
 let phoneCalls: PhoneCall[] = []
 
-export class InMemoryPhoneCallRepository implements AddPhoneCallRepository, ListPhoneCallsRepository {
+export class InMemoryPhoneCallRepository implements AddPhoneCallRepository, ListPhoneCallsRepository, GetPhoneCallByDDDRepository {
   async add(newCall: PhoneCall): Promise<PhoneCall> {
     phoneCalls.push(newCall)
     return newCall
@@ -12,5 +12,9 @@ export class InMemoryPhoneCallRepository implements AddPhoneCallRepository, List
 
   async list(): Promise<PhoneCall[]> {
     return phoneCalls
+  }
+
+  async getByDDD(originDDD: string, destinationDDD: string): Promise<PhoneCall> {
+    return phoneCalls.find(phoneCall => phoneCall.originDDD == originDDD && phoneCall.destinationDDD == destinationDDD)
   }
 }
