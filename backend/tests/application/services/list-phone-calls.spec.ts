cdf6403a0c06d8a4f4771426a7e30f6721a31a29
KPMGE <jesuskevin254@gmail.com>
2022-08-31 00:08:00 -0300

    :white_check_mark: ensure ListPhoneCallsService returns right data

---
+++ backend/tests/application/services/list-phone-calls.spec.ts
@@ -0,0 +1,51 @@
+import { PhoneCall } from "../../../src/domain/entities"
+import { makeFakePhoneCall } from "../../domain/mocks"
+
+interface ListPhoneCallsUseCase {
+  list(): Promise<PhoneCall[]>
+}
+
+interface ListPhoneCallsRepository {
+  list(): Promise<PhoneCall[]>
+}
+
+class ListPhoneCallsRepositoryStub implements ListPhoneCallsRepository {
+  private fakePhoneCall: PhoneCall = {
+    ...makeFakePhoneCall(),
+    id: 'any id'
+  }
+  output = [this.fakePhoneCall, this.fakePhoneCall, this.fakePhoneCall]
+
+  async list(): Promise<PhoneCall[]> {
+    return this.output
+  }
+}
+
+class ListPhoneCallsService implements ListPhoneCallsUseCase {
+  constructor(private readonly repo: ListPhoneCallsRepository) { }
+  async list(): Promise<PhoneCall[]> {
+    return await this.repo.list()
+  }
+}
+
+type SutTypes = {
+  sut: ListPhoneCallsService,
+  repo: ListPhoneCallsRepositoryStub
+}
+
+const makeSut = (): SutTypes => {
+  const repo = new ListPhoneCallsRepositoryStub()
+  const sut = new ListPhoneCallsService(repo)
+
+  return { sut, repo }
+}
+
+describe('list-phone-calls-service', () => {
+  it('should return from repository', async () => {
+    const { sut, repo } = makeSut()
+
+    const phoneCalls = await sut.list()
+
+    expect(phoneCalls).toEqual(repo.output)
+  })
+})
