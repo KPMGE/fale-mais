bfe0bfb2a27ccd9ab2b641a5d01d214ba6ac54d4
KPMGE <jesuskevin254@gmail.com>
2022-08-31 01:40:45 -0300

    :recycle: fix ListPhonePlans test

--- backend/src/presentation/controllers/list-phone-plans.ts
+++ backend/src/presentation/controllers/list-phone-plans.ts
@@ -1,9 +1,9 @@
-import { ListPhoneCallsUseCase } from "../../domain/useCases"
+import { ListPhonePlansUseCase } from "../../domain/useCases"
 import { ok, serverError } from "../helpers"
 import { Controller, HttpResponse } from "../protocols"
 
 export class ListPhonePlansController implements Controller {
-  constructor(private readonly service: ListPhoneCallsUseCase) { }
+  constructor(private readonly service: ListPhonePlansUseCase) { }
   async handle(): Promise<HttpResponse> {
     try {
       const plans = await this.service.list()
