5c84762a3f0a1e4a3ae9df2dbbbc33cff81f7593
KPMGE <jesuskevin254@gmail.com>
2022-08-31 21:55:24 -0300

    :white_check_mark: ensure GetPhonePlanByIdController returns serverError if service throws

--- backend/tests/presentation/controllers/get-phone-plan-by-id.spec.ts
+++ backend/tests/presentation/controllers/get-phone-plan-by-id.spec.ts
@@ -59,4 +59,14 @@ describe('get-phone-plan-by-id-controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new PhonePlanNotFoundError())
   })
+
+  it('should return serverError if service throws other errors', async () => {
+    const { serviceStub, sut } = makeSut()
+    serviceStub.getById = () => { throw new Error('repo error') }
+
+    const httpResponse = await sut.handle({ planId: 'any_plan_id' })
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new Error('repo error'))
+  })
 })
