41588c3e8a514ee221309b633eb4a4d772de5019
KPMGE <jesuskevin254@gmail.com>
2022-09-02 16:42:39 -0300

    :sparkles: finish calls repository integration with postgres

--- backend/src/main/factories/controllers/calculate-call-price.ts
+++ backend/src/main/factories/controllers/calculate-call-price.ts
@@ -1,15 +1,12 @@
 import { CalculateCallPriceService } from "../../../application/services";
-import {
-  InMemoryPhoneCallRepository,
-  InMemoryPhonePlanRepository
-} from "../../../infrastructure/repositories/in-memory";
+import { PostgresPhoneCallRepository, PostgresPhonePlanRepository } from "../../../infrastructure/repositories/postgres";
 import { CalculateCallPriceController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 import { makeCalculateCallPriceValidation } from "../validation";
 
 export const makeCalculateCallPriceController = (): Controller => {
-  const planRepo = new InMemoryPhonePlanRepository()
-  const callRepo = new InMemoryPhoneCallRepository()
+  const planRepo = new PostgresPhonePlanRepository()
+  const callRepo = new PostgresPhoneCallRepository()
   const service = new CalculateCallPriceService(planRepo, callRepo)
   const controller = new CalculateCallPriceController(service, makeCalculateCallPriceValidation())
   return controller
--- backend/src/main/factories/controllers/list-phone-calls.ts
+++ backend/src/main/factories/controllers/list-phone-calls.ts
@@ -1,10 +1,10 @@
 import { ListPhoneCallsService } from "../../../application/services";
-import { InMemoryPhoneCallRepository } from "../../../infrastructure/repositories/in-memory";
+import { PostgresPhoneCallRepository } from "../../../infrastructure/repositories/postgres";
 import { ListPhoneCallsController } from "../../../presentation/controllers";
 import { Controller } from "../../../presentation/protocols";
 
 export const makeListPhoneCallsController = (): Controller => {
-  const repo = new InMemoryPhoneCallRepository()
+  const repo = new PostgresPhoneCallRepository()
   const service = new ListPhoneCallsService(repo)
   const controller = new ListPhoneCallsController(service)
   return controller
