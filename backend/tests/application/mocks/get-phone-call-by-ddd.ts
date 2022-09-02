5963b8d38700ad58b21e48bae21e77fed3e0f991
KPMGE <jesuskevin254@gmail.com>
2022-09-01 22:47:14 -0300

    :white_check_mark: ensure CalculateCallPriceService calculates the call price correctly

--- backend/tests/application/mocks/get-phone-call-by-ddd.ts
+++ backend/tests/application/mocks/get-phone-call-by-ddd.ts
@@ -1,8 +1,9 @@
 import { GetPhoneCallByDDDRepository } from "../../../src/application/repositories"
 import { PhoneCall } from "../../../src/domain/entities"
+import { makeFakePhoneCall } from "../../domain/mocks"
 
 export class GetPhoneCallByDDDRepositoryMock implements GetPhoneCallByDDDRepository {
-  output = null
+  output = { ...makeFakePhoneCall(), id: 'any_id' }
   originDDD = ""
   destinationDDD = ""
   async getByDDD(originDDD: string, destinationDDD: string): Promise<PhoneCall> {
--- backend/tests/application/services/calculate-call-price.spec.ts
+++ backend/tests/application/services/calculate-call-price.spec.ts
@@ -2,14 +2,19 @@ import { GetPhoneCallByDDDRepository, GetPhonePlanByIdRepository } from "../../.
 import { GetPhoneCallByDDDRepositoryMock, GetPhonePlanByIdRepositoryMock } from "../mocks"
 
 interface CalculateCallPriceUseCase {
-  calculate(input: CalculateCallPriceUseCase.Props): Promise<number>
+  calculate(input: CalculateCallPriceUseCase.Props): Promise<CalculateCallPriceUseCase.Result>
 }
 
 export namespace CalculateCallPriceUseCase {
   export type Props = {
-    phonePlanId: string,
-    originDDD: string,
+    phonePlanId: string
+    amountMinutes: number
+    originDDD: string
     destinationDDD: string
+  }
+  export type Result = {
+    priceWithPlan: number
+    priceWithoutPlan: number
   }
 }
 
@@ -19,17 +24,32 @@ class CalculateCallPriceService implements CalculateCallPriceUseCase {
     private readonly getPhoneCallRepo: GetPhoneCallByDDDRepository
   ) { }
 
-  async calculate({ phonePlanId, originDDD, destinationDDD }: CalculateCallPriceUseCase.Props): Promise<number> {
-    await this.getPhonePlanRepo.getById(phonePlanId)
-    await this.getPhoneCallRepo.getByDDD(originDDD, destinationDDD)
-    return null
+  async calculate(input: CalculateCallPriceUseCase.Props): Promise<CalculateCallPriceUseCase.Result> {
+    const { phonePlanId, originDDD, destinationDDD, amountMinutes } = input
+
+    const { durationInMinutes } = await this.getPhonePlanRepo.getById(phonePlanId)
+    const { pricePerMinute } = await this.getPhoneCallRepo.getByDDD(originDDD, destinationDDD)
+
+    const priceWithoutPlan = pricePerMinute * amountMinutes
+    let priceWithPlan = 0
+
+    if (durationInMinutes < amountMinutes) {
+      const difference = amountMinutes - durationInMinutes
+      priceWithPlan = difference * pricePerMinute
+    }
+
+    return {
+      priceWithoutPlan,
+      priceWithPlan
+    }
   }
 }
 
 const makefakeCalculatePriceInput = (): CalculateCallPriceUseCase.Props => ({
   phonePlanId: 'any_phone_plan_id',
   originDDD: 'any_origin_ddd',
-  destinationDDD: 'any_destination_ddd'
+  destinationDDD: 'any_destination_ddd',
+  amountMinutes: 60
 })
 
 describe('calculate-call-price-service', () => {
@@ -43,5 +63,42 @@ describe('calculate-call-price-service', () => {
     expect(getCallRepo.originDDD).toBe('any_origin_ddd')
     expect(getCallRepo.destinationDDD).toBe('any_destination_ddd')
     expect(getPlanRepo.input).toBe('any_phone_plan_id')
+  })
+
+  it('should calculate the price with and wihtout plan', async () => {
+    const getCallRepo = new GetPhoneCallByDDDRepositoryMock()
+    const getPlanRepo = new GetPhonePlanByIdRepositoryMock()
+    const sut = new CalculateCallPriceService(getPlanRepo, getCallRepo)
+
+    // plan duration of 60 minutes
+    getPlanRepo.output.durationInMinutes = 60
+
+    // call duration of 20 minutes
+    const fakeInputCase1 = { ...makefakeCalculatePriceInput(), durationInMinutes: 20 }
+
+    // withoutPlan, the price is the amountMinutes times the price per minute
+    const withoutPlanCase1 = getCallRepo.output.pricePerMinute * fakeInputCase1.amountMinutes
+    // as the plan has a duration greater than the amountMinutes, the price with the plan is 0
+    const withPlanCase1 = 0
+
+    const resultCase1 = await sut.calculate(fakeInputCase1)
+
+    expect(resultCase1.priceWithPlan).toBe(withPlanCase1)
+    expect(resultCase1.priceWithoutPlan).toBe(withoutPlanCase1)
+
+    //  plan duration of 20 minutes 
+    getPlanRepo.output.durationInMinutes = 20
+    // call duration of 60 minutes
+    const fakeInputCase2 = { ...makefakeCalculatePriceInput(), durationInMinutes: 60 }
+
+    // the price without the plan is the same, the amountMinutes times the price per minute
+    const withoutPlanCase2 = getCallRepo.output.pricePerMinute * fakeInputCase2.amountMinutes
+    // the price with the plan is the amountMinutes minus the durationInMinutes of the plan, times the price per minute
+    const withPlanCase2 = (60 - 20) * getCallRepo.output.pricePerMinute
+
+    const resultCase2 = await sut.calculate(fakeInputCase2)
+
+    expect(resultCase2.priceWithPlan).toBe(withPlanCase2)
+    expect(resultCase2.priceWithoutPlan).toBe(withoutPlanCase2)
   })
 })
